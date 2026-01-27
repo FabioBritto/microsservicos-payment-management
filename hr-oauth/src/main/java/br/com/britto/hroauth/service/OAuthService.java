cddebec85f6fab58c040fec8830d5684caf30095
FabioBritto <fabio.tritono@gmail.com>
2026-01-27 18:48:23 -0300

    feat: login with jwt token

--- hr-oauth/src/main/java/br/com/britto/hroauth/service/OAuthService.java
+++ hr-oauth/src/main/java/br/com/britto/hroauth/service/OAuthService.java
@@ -3,13 +3,24 @@ package br.com.britto.hroauth.service;
 import br.com.britto.hroauth.feignclients.UserFeignClients;
 import br.com.britto.hroauth.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class OAuthService {
+public class OAuthService implements UserDetailsService {
 
     @Autowired
     private UserFeignClients userFeignClients;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User existingUser = userFeignClients.findByEmail(email).getBody();
+        User user = new User(existingUser.getId(),existingUser.getname(), existingUser.getEmail(), existingUser.getPassword());
+        user.setRoles(existingUser.getRoles());
+        return user;
+    }
 
     public User findByEmail(String email) {
         User existingUser = userFeignClients.findByEmail(email).getBody();
