c13c2fe417cd8478e3bdee9c9203f1680cc7dc59
FabioBritto <fabio.tritono@gmail.com>
2026-01-29 14:07:15 -0300

    feat: zuul authorization

--- hr-user/src/main/resources/data.sql
+++ hr-user/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Fabio Britto', 'fabio.tritono@gmail.com', '$2a$12$/0lNQtllHslai9IHmR1S9uB6hFDpKSITsYLj9X41ehMOwe/8iUOdOddd');
+INSERT INTO tb_user (name, email, password) VALUES ('Fabio Britto', 'fabio.tritono@gmail.com','$2a$12$hPU7bU1UYQJbBQXDmf145.6BLjXls8V.p/q8yb4BOd0wKBIDtlUWG');
 INSERT INTO tb_user (name, email, password) VALUES ('Priscila Britto', 'priscila.britto@gmail.com', '$2a$12$xGDo6r1ETkbOlUeXab4sL..2/b8m0pVwBOoZEhs1sShK77sQOa4hq');
 
 -- INSERT INTO tb_user (id, name, email, password) VALUES (null,'Fabio Britto', 'fabio.tritono@gmail.com', '$2a$12$/0lNQtllHslai9IHmR1S9uB6hFDpKSITsYLj9X41ehMOwe/8iUOdOddd');
